6c33660083b790eca1baebb204e18479ad60f00b
sagar <variyasagar2@gmail.com>
2022-01-27 21:44:28 +0530

    SUper admin done

--- assets/js/custome_superadmin.js
+++ assets/js/custome_superadmin.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
 
     get_all_marriages_ajax();
     $(document).on('keypress', '#serchmrg', function () {
-        console.log($(this).val());
+        // console.log($(this).val());
         var that = this, value = $(this).val();
         if (x != null) x.abort();
         get_all_marriages_ajax(value);
@@ -33,7 +33,12 @@ $(document).ready(function () {
         var frm = that.serializeArray();
         var data = {};
         $.each(frm, function (k, v) {
-            data[v.name] = v.value;
+            if (v.name == 'secondary_color') {
+                data[v.name] = v.value.split("|")
+            } else {
+                data[v.name] = v.value;
+            }
+
         })
         settings.data = JSON.stringify(data);
         // console.log( data);
@@ -51,4 +56,83 @@ $(document).ready(function () {
     $(document).on('click', '#save_btn', function () {
         addmrg();
     });
+
+    function getallimage() {
+        settings.data = JSON.stringify({});
+        settings.url = BASE_URL + "get_all_wardrobe"
+        $.ajax(settings).done(function (response) {
+            if (getresopncesuccess(response)) {
+                var wrdhrml = $(document).find("#wardrode-img");
+                var html = '';
+                $.each(response.data, function (k, v) {
+                    html += htmlimg(v);
+                })
+                // console.log(html)
+                wrdhrml.html(html);
+            }
+        })
+    }
+
+    getallimage();
+
+    function dalatewar(id) {
+
+        settings.data = JSON.stringify({wardrobe_ids: [id]});
+        settings.url = BASE_URL + "delete_wardrobe"
+        $.ajax(settings).done(function (response) {
+            if (getresopncesuccess(response)) {
+                getallimage();
+            }
+        });
+    }
+
+    $(document).on('click', '.wardrode-box .minus', function () {
+        var id = $(this).data('id');
+        if (confirm("Are you sure to remove?")) {
+            dalatewar(id);
+        }
+    })
+
+    function add_image() {
+        var formData = new FormData();
+        datfrom = $(document).find('#wardrobe_image')
+        formData.append('wardrobe_image', datfrom[0].files[0]);
+        // console.log(datfrom[0].files[0]);
+        //
+        let settingclon = settings;
+        settingclon.processData = false
+        settingclon.contentType = false
+        delete settingclon.headers
+        settingclon.mimeType = "multipart/form-data"
+        settingclon.url = BASE_URL + 'add_wardrobes'
+        settingclon.data = formData
+        settingclon.success = function (response) {
+
+            $("#exampleModal1").modal('hide');
+            $("#image_wardrode_form")[0].reset()
+            getallimage();
+        }
+        $.ajax(settingclon);
+    }
+
+    $(document).on('click', '#image_save_btn', function () {
+        add_image()
+    })
+
+    function changests(id, sts, tht) {
+
+        settings.data = JSON.stringify({marriage_id: id, is_active: sts});
+        settings.url = BASE_URL + "update_marriage_is_active"
+        $.ajax(settings).done(function (response) {
+            if (getresopncesuccess(response)) {
+            }
+        });
+    }
+
+    $(document).on("change", ".ckbox", function () {
+        var id = $(this).data('id');
+        var sts = $(this).prop('checked')
+        // alert($(this).prop('checked'))
+        changests(id, sts, $(this))
+    })
 })
