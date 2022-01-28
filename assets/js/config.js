31e396ebc9ec74b0cd7784ad8255bfdef525ebdc
sagar <variyasagar2@gmail.com>
2022-01-28 20:21:12 +0530

    admin module

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,31 +3,44 @@ const BASE_URL = 'http://34.222.156.42:8081/'
 const url = 'http://localhost/wedding/'
 
 // const urlParams = ;
+var urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+cnt = urlParams.get('cnt') || 0;
+// console.log(cnt);
+// console.log(id,localStorage.getItem('id'))
+if (cnt > 5) {
+    localStorage.clear();
+}
 function checkLogin(redirectURL = null, redirectlogin = false) {
     userdetails = localStorage.getItem('userDetails') || localStorage.getItem('is_admin')
     if (userdetails == 'SuperAdmin') {
+        // alert();
         // alert(redirectURL || url + "super_admin");
-        if (!redirectlogin) {
-            alert('233242');
+        var urlParams = new URLSearchParams(window.location.search);
+
+        if (!redirectlogin ) {
+            // alert('233242');
+            document.location.href = redirectURL || url + "super_admin";
+        }
+
+        var loc = window.location;
+        var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
+        pathName =pathName.split("/")
+        pathName= $.grep(pathName,function(n){ return n != '' || n });
+        // pathName=    loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+
+        if ( id == null && (pathName[ pathName.length -1 ] != 'super_admin' || (pathName[ pathName.length -1 ] == 'index.html') && pathName[ pathName.length -2 ] !='super_admin' )) {
+            // alert();
             document.location.href = redirectURL || url + "super_admin";
         }
     } else if (userdetails != null && userdetails != '') {
-        // alert(redirectURL || url + "admin")
         if (redirectlogin) {
-            var urlParams = new URLSearchParams(window.location.search);
-            id = urlParams.get('id');
-            cnt = urlParams.get('cnt') || 0;
-            console.log(cnt);
-            // console.log(id,localStorage.getItem('id'))
-            if (cnt > 5) {
-                localStorage.clear();
-            }
             cnt++;
             if (localStorage.getItem('id') != id) {
-                document.location.href = redirectURL || url + "admin?cnt=" + cnt + "&id=" + id;
+                document.location.href = redirectURL || url + "admin/?cnt=" + cnt + "&id=" + id;
             }
         } else {
-            document.location.href = redirectURL || url + "admin?id=" + localStorage.getItem('id');
+            document.location.href = redirectURL || url + "admin/?id=" + localStorage.getItem('id');
         }
 
     } else if (redirectlogin) {
--- assets/js/custome_adminhome.js
+++ assets/js/custome_adminhome.js
@@ -1,2 +1,66 @@
-// alert();
+checkLogin(null, true);
 
+
+function home() {
+    settings.url = BASE_URL + 'get_single_marriages';
+
+    settings.data = settings.data = JSON.stringify({marriage_id: id});
+    ;
+    $.ajax(settings).done(function (response) {
+        if (getresopncesuccess(response)) {
+            // console.log(response.data)
+            let data = response.data
+            response.data.wedding_date = cutomdate(response.data.wedding_date, 'dd/MM/yyyy')
+            $.each(response.data, function (k, v) {
+                var x = $(document).find("#d_" + k);
+                if (x.length > 0) {
+                    x.html(v);
+                }
+            })
+            if (data.marriage_logo)
+                $(document).find('#profile_pic').css('background', 'url(' + BASE_URL + data.marriage_logo + ')');
+            if (data.is_dark) {
+                $(document).find('#d_primary').addClass('primary-color');
+            } else {
+                $(document).find('#d_primary').addClass('white-color');
+            }
+            color = '#' + data.secondary_color.join(',#');
+            // console.log(color)
+            $(document).find('#d_secondory').css('background', 'linear-gradient(' + color + ')');
+            var bannerhtml = '';
+            $.each(data.banner, function (k, v) {
+                bannerhtml += banner(v);
+            })
+            // console.log(bannerhtml);
+            $(document).find('#banner_display').html(bannerhtml);
+            // console.log(html,printHtml);
+        }
+    });
+}
+home();
+
+function add_image() {
+    var formData = new FormData();
+    datfrom = $(document).find('#wardrobe_image')
+    formData.append('wardrobe_image', datfrom[0].files[0]);
+    // console.log(datfrom[0].files[0]);
+    //
+    let settingclon = settings;
+    settingclon.processData = false
+    settingclon.contentType = false
+    delete settingclon.headers
+    settingclon.mimeType = "multipart/form-data"
+    settingclon.url = BASE_URL + 'add_wardrobes'
+    settingclon.data = formData
+    settingclon.success = function (response) {
+
+        $("#exampleModal1").modal('hide');
+        $("#image_wardrode_form")[0].reset()
+        getallimage();
+    }
+    $.ajax(settingclon);
+}
+
+$(document).on('click', '#image_save_btn', function () {
+    add_image()
+})
